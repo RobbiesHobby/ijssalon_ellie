d772abd79590bb8640677dc6d8e53e2a02cab2db
Robert Swier <dionysus@wxs.nl>
2023-09-30 10:23:12 +0200

    functies toegevoegd

---
+++ test.py
@@ -0,0 +1,8 @@
+def mijn_functie(mijn_lijst):
+    totaal = 0
+    for nr in mijn_lijst:
+        totaal += nr
+    return totaal
+mijn_lijst = [1,3,5,7]
+mijn_functie(mijn_lijst = [1,2,4,6])
+print(totaal)
--- tijdelijk.py
+++ tijdelijk.py
@@ -6,16 +6,16 @@ def print_aanbieding():
         "vanille" : 4,
         "chocolade" : 5
     }
-    aanbieding = prijzen["aardbei"] * 0.8 # subvraag 3 (met wijziging cf. toelichting door Peter bij jhuiswerk 6)
-    reclame_tekst = f"Vandaag in de aanbieding: aardbei-ijs, 1 liter - slechts €{aanbieding}" # subvraag 4
-    reclame_tekst2 = reclame_tekst[:62] # subvraag 5
-    reclame_tekst3 = reclame_tekst2.upper() # subvraag 6
-    reclame_tekst4 = reclame_tekst3.split() # subvraag 7
+    aanbieding = prijzen["aardbei"] * 0.8
+    reclame_tekst = f"Vandaag in de aanbieding: aardbei-ijs, 1 liter - slechts €{aanbieding}"
+    reclame_tekst2 = reclame_tekst[:62]
+    reclame_tekst3 = reclame_tekst2.upper()
+    reclame_tekst4 = reclame_tekst3.split()
     for el in reclame_tekst4:
-        # print(el) # subvraag 8
-        if len(el)>=5: # subvraag 10
+        if len(el)>=5:
             print(el)
         else:
-            print(el.lower()) # subvraag 9
+            print(el.lower())
+
 decoreer("Aanbieding")
 print_aanbieding()
