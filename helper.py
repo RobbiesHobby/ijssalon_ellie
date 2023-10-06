4b518d59aa9f778d57234f722070548dc0f1c428
Robert Swier <dionysus@wxs.nl>
2023-10-06 19:38:04 +0200

    boekhouding en presentatie en csv toegevoegd

--- helper.py
+++ helper.py
@@ -5,3 +5,20 @@ def decoreer(tekst=""):
     print(f"* {tekst} *")
     print(lengte * "*")
     print()
+
+def fooi_pp(bedrag, personen):
+    bedrag_pp = bedrag / personen
+    return f"Het bedrag per persoon is {bedrag_pp}"
+
+def onderstreep(tekst = ""):
+    uit = []
+    uit.append(tekst)
+    uit.append(len(tekst) * "=")
+    return uit
+
+def som(omzet):
+    values = omzet.values()
+    resultaat = 0
+    for ijstype in values:
+        resultaat += ijstype
+    return resultaat
---
+++ presentatie.py
@@ -0,0 +1,7 @@
+def presenteer(mijn_verkoop, totaal):
+    # pass
+
+    for (k, v) in mijn_verkoop.items():
+        print(f"{k} : {v} euro")
+    print (26 * "=")
+    print(f"Totaal : {totaal} euro")  
